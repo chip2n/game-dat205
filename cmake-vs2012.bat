4afaec9b92de9b7d2f699d2ad9c733972731dc38
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-11-10 20:22:48 +0100

    Added GLFW source code.

--- cmake-vs2012.bat
+++ cmake-vs2012.bat
@@ -1,3 +1,4 @@
 mkdir build
 cd build
 cmake -G "Visual Studio 11" ../
+cd ..
--- test.cpp
+++ test.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+
+void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(int argc, const char *argv[]) {
-    std::cout << "Hello World";
-	std::string temp;
-	std::cin.get();
+	GLFWwindow* window;
+	glfwSetErrorCallback(error_callback);
+
+	if(!glfwInit())
+		exit(EXIT_FAILURE);
+
+	 window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
+
+	if(!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
